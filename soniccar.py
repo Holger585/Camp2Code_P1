7ef7290e280273f463c4787e16b83d802f6fdc02
Christof <christof.goedderth@gmx.de>
2025-01-09 14:27:41 +0100

    loggen hinzugefügt

--- soniccar.py
+++ soniccar.py
@@ -1,6 +1,7 @@
 from basecar import BaseCar
 from basisklassen import Ultrasonic
 import time
+import random
 
 class SonicCar(BaseCar):
     def __init__(self):
@@ -16,28 +17,43 @@ class SonicCar(BaseCar):
     def fahrmodus_3(self,min_dist = 8,speed = 50, angle=90):
         
         self.drive(speed,angle)
+        self.loggen(self.get_distance,self._speed,self._steering_angle)
         while True:
             distance = self.get_distance
-            print(f"Hindernis erkannt in  {distance} cm.")
             if 0 < distance < min_dist:
                 self.stop()
-                print(f"Hindernis erkannt in  {distance} cm.")
+                self.loggen(self.get_distance,self._speed,self._steering_angle)
                 break
             time.sleep(0.2)
+        self.stop()
     
-        
-    def fahrmodus_4(self,min_dist = 8, speed = 30, angle=90, lenken = 135):
-        #while True:
-        self.fahrmodus_3()
-        self.drive(0,lenken)
-        self.drive(-speed)
-        time.sleep(1)
-        self.fahrmodus_3()
+    
+    def fahrmodus_4(self, speed = 30, lenken = 135):
+        start_time=time.time
+        while time.time()-start_time < 60: # Schleife endet nach 60 Sekunden
+            self.fahrmodus_3(angle= random.randint(45, 135))
+            self.drive(0,lenken)
+            self.loggen(self.get_distance,self._speed,self._steering_angle)
+            self.drive(-speed)
+            time.sleep(1)
+            self.stop()
+            self.loggen(self.get_distance,self._speed,self._steering_angle)
+        self.stop()
+    
+    def loggen(self, distance, speed, steering_angle):
+            log.append({
+            "distance": distance,
+            "speed": speed,
+            "steering_angle": steering_angle
+            })
+            print(f"Gemessene Entfernung: {distance} cm, Geschwindigkeit: {speed}, Lenkwinkel: {steering_angle}")
+
 
 
 
                 
 if __name__ == "__main__":
     car = SonicCar() 
+    log = []
     car.fahrmodus_4()
     print(car.get_distance)
