092fe49a3871996e1417f0348874f634043c7fda
Christof <christof.goedderth@gmx.de>
2025-01-09 13:55:55 +0100

    Fahrmodus 4 ergänzt

--- soniccar.py
+++ soniccar.py
@@ -16,7 +16,6 @@ class SonicCar(BaseCar):
     def fahrmodus_3(self,min_dist = 8,speed = 50, angle=90):
         
         self.drive(speed,angle)
-        
         while True:
             distance = self.get_distance
             print(f"Hindernis erkannt in  {distance} cm.")
@@ -25,8 +24,20 @@ class SonicCar(BaseCar):
                 print(f"Hindernis erkannt in  {distance} cm.")
                 break
             time.sleep(0.2)
+    
+        
+    def fahrmodus_4(self,min_dist = 8, speed = 30, angle=90, lenken = 135):
+        #while True:
+        self.fahrmodus_3()
+        self.drive(0,lenken)
+        self.drive(-speed)
+        time.sleep(1)
+        self.fahrmodus_3()
+
+
+
                 
 if __name__ == "__main__":
     car = SonicCar() 
-    car.fahrmodus_3()
+    car.fahrmodus_4()
     print(car.get_distance)
