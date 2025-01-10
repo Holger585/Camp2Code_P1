be4543f307b0abea5067f6c2799a34bf89f6377e
Dominik <dominik.evers@gmx.de>
2025-01-10 11:35:31 +0100

    Update

--- app_data.py
+++ app_data.py
@@ -2,8 +2,10 @@ import pandas as pd
 
 # CSV-Datei einlesen
 df = pd.read_csv('fahrmodus_log.csv')
-
+df['Fahrstrecke'] = df['Geschwindigkeit'] * 10
 print(f"Fahrzeit : {df['Zeit'].max()} s")
 print(f"Geschwindigkeit min.: {df['Geschwindigkeit'].min()} km/h")
 print(f"Geschwindigkeit max.: {df['Geschwindigkeit'].max()} km/h")
 print(f"Geschwindigkeit mean: {df['Geschwindigkeit'].mean()} km/h")
+print(df)
+
