afbe923d03185a364c5ed4bf3ed35b70594d6c31
Dominik <dominik.evers@gmx.de>
2025-01-10 12:14:11 +0100

    Fahrtsrecke hinzugefügt

--- app_data.py
+++ app_data.py
@@ -2,10 +2,13 @@ import pandas as pd
 
 # CSV-Datei einlesen
 df = pd.read_csv('fahrmodus_log.csv')
-df['Fahrstrecke'] = df['Geschwindigkeit'] * 10
+df['Geschwindigkeit'] = abs(df['Geschwindigkeit'])
+df['Fahrstrecke'] = df['Geschwindigkeit'] * df['Zeit'].diff().fillna(0)
+
 print(f"Fahrzeit : {df['Zeit'].max()} s")
 print(f"Geschwindigkeit min.: {df['Geschwindigkeit'].min()} km/h")
 print(f"Geschwindigkeit max.: {df['Geschwindigkeit'].max()} km/h")
 print(f"Geschwindigkeit mean: {df['Geschwindigkeit'].mean()} km/h")
+print(f"Fahrstrecke gesamt: {df['Fahrstrecke'].sum()} km")
 print(df)
 
