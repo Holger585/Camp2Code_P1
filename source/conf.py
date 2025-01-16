93b298f57577126bc95bf1a99fa172db636dbb72
Holger585 <holger.niemeyer@gmx.net>
2025-01-16 14:25:43 +0100

    neu

---
+++ source/conf.py
@@ -0,0 +1,33 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Camp2Code Projektphase 1'
+copyright = '2025, Team 1'
+author = 'Team 1'
+release = 'V1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'numpydoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'de'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('/home/pi/Bootcamp/Camp2Code_P1/'))  # Passe den Pfad zu deinem Quellcode an
